7e4cee4630eb5356d8f808657ed078ba708f0bef
BakedCheese <48770243+BakedCheese@users.noreply.github.com>
2021-12-23 00:51:11 +0100

    first changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,184 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container text-center">
+    <div class="mt-4 mx-4 mx-md-0">
+      <div class="row">
+        <div class="bblock">
+          <h2 class="w-text fw-bolder spacing">Hello . Osu . Morge</h2>
+        </div>
+        <div class="wblock">
+          <p class="g-text">
+            My name is Tim van Daalen, I'm a Dutch software Engineer student at
+            The Hague University of Applied Sciences. I like playing and making
+            games, exploring the world of the unknown, traveling and being a
+            nerd.
+          </p>
+        </div>
+        <div class="right-side">
+          <div class="button button-under-object">About me</div>
+        </div>
+        <div class="margin"></div>
+        <div class="margin"></div>
+        <div class="margin head">
+          <div class="w-text fw-bold spacing">Latest Collections</div>
+        </div>
+        <div class="some-collections">
+          <img src="" style="object-position: center" alt="example1" />
+          <div class="margin"></div>
+          <img src="" alt="example2" />
+        </div>
+        <div class="right-side">
+          <div class="button button-under-object">All collections</div>
+        </div>
+        <div class="margin"></div>
+        <div class="margin"></div>
+        <footer class="footer">Made by BakedCheese</footer>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.some-collections {
+  padding: 0px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.some-collections > img {
+  border-radius: 50px;
+  width: 100%;
+  height: 280px;
+  object-fit: cover;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.spacing {
+  letter-spacing: 3px;
 }
-a {
-  color: #42b983;
+
+.bblock {
+  background-color: #1e1e1e;
+  border-radius: 50px;
+  width: 100%;
+  max-width: 500px;
+  height: 183px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
+  padding-bottom: 20px;
+}
+
+.wblock {
+  color: rgba(30, 30, 30, 0.8);
+  background-color: #f9f9f9;
+  border-radius: 50px;
+  width: 100%;
+  max-width: 500px;
+  height: 184px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
+  margin-top: -55px;
+  z-index: 2;
+  padding: 23px 30px 17px 30px;
+  font-size: 1.2rem;
+  font-weight: 500;
+  line-height: 1.6rem;
+}
+
+@media only screen and (max-width: 600px) {
+  .wblock {
+    padding: 26px 28px 14px 28px;
+    line-height: 1.4rem;
+    font-size: 0.9rem;
+  }
+}
+@media only screen and (max-width: 300px) {
+  .wblock {
+    padding: 26px 28px 14px 28px;
+    line-height: 1rem;
+    font-size: 0.8rem;
+  }
+}
+
+.head {
+  background-color: #1e1e1e;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  height: 100px;
+  border-radius: 50px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
+  font-size: 1.2rem;
+}
+.button {
+  background-color: #2a6e47;
+  border-radius: 0px 32px 32px 32px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
+  height: 50px;
+  width: 200px;
+  color: #f9f9f9;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  font-weight: 500;
+  font-size: 1.3rem;
+}
+.button-under-object {
+  margin-top: 15px;
+}
+.right-side {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+}
+.footer {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  max-width: 500px;
+  height: 50px;
+  border-radius: 50px 50px 0px 0px;
+  background-color: #f9f9f9;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
+  color: rgba(30, 30, 30, 0.8);
+  font-weight: 500;
+}
+
+@media only screen and (max-width: 600px) {
+  .footer {
+    padding-top: 3px;
+  }
+}
+.container {
+  max-width: 450px;
+}
+.margin {
+  margin-bottom: 25px;
+}
+
+.object-in-bar {
+  margin-bottom: -4px;
+}
+
+button:hover {
+  text-decoration: underline;
+}
+
+.b-text {
+  color: black;
+}
+.w-text {
+  color: white;
+}
+.g-text {
+  color: #1e1e1ec7;
 }
 </style>
