bc7af2fae2a0dee29042556588dae2e0181a3ed0
BakedCheese <48770243+BakedCheese@users.noreply.github.com>
2021-12-30 17:07:07 +0100

    added a host

--- vue.config.cjs
+++ vue.config.cjs
@@ -2,6 +2,6 @@ module.exports = {
   devServer: {
     clientLogLevel: "info",
     compress: true,
-    public: "portfolio-app-6qg7c.ondigitalocean.app",
+    public: ["portfolio-app-6qg7c.ondigitalocean.app", "thewoodytree.xyz"],
   },
 };
