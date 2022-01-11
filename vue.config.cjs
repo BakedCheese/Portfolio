565a11d598488240e4b3d21d9130315ecdb4ca7f
BakedCheese <48770243+BakedCheese@users.noreply.github.com>
2022-01-11 21:37:46 +0100

    Update vue.config.cjs

--- vue.config.cjs
+++ vue.config.cjs
@@ -2,9 +2,6 @@ module.exports = {
   devServer: {
     clientLogLevel: "info",
     compress: true,
-    allowedHosts: [
-      "portfolio-app-6qg7c.ondigitalocean.app",
-      "bakedcheesegames.com",
-    ],
+    allowedHosts: ["portfolio-app-6qg7c.ondigitalocean.app", "bakedcheese.nl"],
   },
 };
